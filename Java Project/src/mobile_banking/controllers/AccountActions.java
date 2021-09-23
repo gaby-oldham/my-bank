7d089af70c64f067f7ff06ce392db10a7d78dd3f
gaby-oldham <gabrielle.oldham@accenture.com>
2021-09-23 11:37:01 +0100

    Updated and annotated code.

--- Java Project/src/mobile_banking/controllers/AccountActions.java
+++ Java Project/src/mobile_banking/controllers/AccountActions.java
@@ -1,5 +1,34 @@
 package mobile_banking.controllers;
+import mobile_banking.models.*;
+import mobile_banking.views.*;
+import java.util.*;
+@SuppressWarnings("unused")
 
 public class AccountActions {
+/* creates account action controller class
+   this class will allow the user to view or create an account*/
+
+    public static boolean createAccount(User u){
+    /* creates new method
+       this method will allow the user to open a new account within the system */
+        return false;
+        /* boolean return value
+           this is so we can validate whether an account has been successfully created or not */
+    }
+
+    public static void viewAccounts(ArrayList<Account> accList) {
+    /* creates new method
+       this method will allow the user to view the accounts they already have open within the system */
+        String[] accountNames = new String[accounts.size()];
+        for(Account a : accList){
+        // for every account in the list of accounts...
+            accountNames[accList.indexOf(a)] = a.getAccountName();
+            // creates a list of all the user's account names
+        }
+        int accountSelected = AccountActionsMenu.displayAccounts(accountNames) - 1;
+        // sets the value of variable accountSelected to the index of the account the user has selected
+        Account selectedAccount = accList.get(accountSelected);
+        // sets the value of variable selectedAccount to the account selected by the user
+    }
 
 }
--- Java Project/src/mobile_banking/views/AccountActionsMenu.java
+++ Java Project/src/mobile_banking/views/AccountActionsMenu.java
@@ -1,6 +1,27 @@
 package mobile_banking.views;
+import java.util.Scanner;
+@SuppressWarnings("unused")
 
 public class AccountActionsMenu {
+/* creates account action menu view class
+   this class will allow the user to choose an account */
+
+    Scanner scn = new Scanner(System.in);
+    // creates a new instance of the scanner class
+
+    public static int displayAccounts(String[] accounts){
+    /* creates new method
+       this method will allow the user to choose an account */
+        System.out.println("Please select one of your accounts from the list below:");
+        // prompt - outputted to the user
+        for(int i = 1; i < accounts.length + 1; i++){
+        // this loop will run until it has outputted every item of the accounts list
+            System.out.println(i + " - " + accounts[i - 1]);
+            // outputs the item of the accounts list with the index corresponding to the value of i
+        }
+        return Integer.parseInt(new Scanner(System.in).nextLine());
+        // the return value of this method will be the corresponding value to the account selected by the user
+    }
 
     /*
 
