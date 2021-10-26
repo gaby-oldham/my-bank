10dd99be0504b547078b3bb0d9aee092faa72f57
gaby-oldham <gabrielle.oldham@accenture.com>
2021-10-26 11:17:56 +0100

    After compiling.

--- Java Project/src/my-bank-master/src/main/java/com/abc/Account.java
+++ Java Project/src/my-bank-master/src/main/java/com/abc/Account.java
@@ -79,6 +79,7 @@ public class Account {
         }
     }
 
+
     public double sumTransactions() {
        return checkIfTransactionsExist(true);
     }
--- Java Project/src/my-bank-master/src/main/java/com/abc/Customer.java
+++ Java Project/src/my-bank-master/src/main/java/com/abc/Customer.java
@@ -45,6 +45,7 @@ public class Customer {
         accountTwo.deposit(amount);
     }
 
+
     public String getName() {
         return name;
     }
